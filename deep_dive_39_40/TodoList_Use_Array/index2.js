911b46a75081566d71782b4e1a704355a4eb85f7
sjy <jen.jyseo@gmail.com>
2022-03-20 22:57:03 +0900

    commit

--- deep_dive_39_40/TodoList_Use_Array/index2.js
+++ deep_dive_39_40/TodoList_Use_Array/index2.js
@@ -1,1 +1,5 @@
 const form = document.getElementById("form");
+const list = document.getElementById('list');
+const input = document.getElemetById('input');
+const button = document.getElementById('button');
+
