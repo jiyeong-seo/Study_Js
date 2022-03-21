765a90ac2abb9b677f54747117c7c95e3500ad7e
sjy <jen.jyseo@gmail.com>
2022-03-21 21:27:28 +0900

    commit

--- deep_dive_39_40/TodoList_Use_Array/index2.js
+++ deep_dive_39_40/TodoList_Use_Array/index2.js
@@ -1,5 +1,14 @@
-const form = document.getElementById("form");
-const list = document.getElementById('list');
-const input = document.getElemetById('input');
-const button = document.getElementById('button');
+const form = document.getElementById("create-list-from");
+const list = document.getElementById("list");
+const input = document.getElementById("create-list-from__input");
+const button = document.getElementById("create-list-form__button");
+let todos = [];
+let id = 1;
 
+// 추가 버튼 클릭시 인풋값을 받는 함수
+const handelGetValue = (e) => {
+  const inputValue = e.target.previousElementSibling.value;
+  console.log(inputValue);
+};
+
+button.addEventListener("click", handelGetValue);
